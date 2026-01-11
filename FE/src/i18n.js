3a5ee917ca089ef4ff47e18d05c6d96248500155
DangDangHocCode <haidang972004@gmail.com>
2026-01-12 02:37:08 +0700

    feat(fe): update i18n resources for admin and common modules

--- FE/src/i18n.js
+++ FE/src/i18n.js
@@ -6,6 +6,7 @@ import enHome from "@/locales/en/home.json";
 import enAdmin from "@/locales/en/admin.json";
 import enMyProjects from "@/locales/en/myProjects.json";
 import enAuth from "@/locales/en/auth.json";
+import enChatWidget from "@/locales/en/chatWidget.json";
 import viCommon from "@/locales/vi/common.json";
 import viHeader from "@/locales/vi/header.json";
 import viFooter from "@/locales/vi/footer.json";
@@ -14,22 +15,24 @@ import viHome from "@/locales/vi/home.json";
 import viAdmin from "@/locales/vi/admin.json";
 import viMyProjects from "@/locales/vi/myProjects.json";
 import viAuth from "@/locales/vi/auth.json";
+import viChatWidget from "@/locales/vi/chatWidget.json";
 import jaCommon from "@/locales/ja/common.json";
 import jaHome from "@/locales/ja/home.json";
 import jaAuth from "@/locales/ja/auth.json";
 import jaHeader from "@/locales/ja/header.json";
 import jaFooter from "@/locales/ja/footer.json";
 import jaAdmin from "@/locales/ja/admin.json";
+import jaChatWidget from "@/locales/ja/chatWidget.json";
 
 const messages = {
     en: {
         ...enCommon,
         ...enHeader,
-        ...jaAdmin,
         ...enFooter,
         ...enHome,
         ...enAdmin,
         ...enMyProjects,
+        ...enChatWidget,
         auth: enAuth
     },
     vi: {
@@ -40,6 +43,7 @@ const messages = {
         ...viHome,
         ...viAdmin,
         ...viMyProjects,
+        ...viChatWidget,
         auth: viAuth
     },
     ja: {
@@ -48,6 +52,7 @@ const messages = {
         ...jaHeader,
         ...jaFooter,
         ...jaAdmin,
+        ...jaChatWidget,
         auth: jaAuth
     }
 }
