e080772053defa325281078d6aaecc41b19969fd
DangDangHocCode <haidang972004@gmail.com>
2026-01-14 02:04:59 +0700

    feat(be): add chat feedback repository

--- management_system/src/main/java/com/management_system/repository/ChatFeedbackRepository.java
+++ management_system/src/main/java/com/management_system/repository/ChatFeedbackRepository.java
@@ -1,13 +1,13 @@
 package com.management_system.repository;
 
-import com.management_system.entity.ChatFeedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.management_system.entity.ChatFeedback;
 
 @Repository
 public interface ChatFeedbackRepository extends JpaRepository<ChatFeedback, UUID> {
