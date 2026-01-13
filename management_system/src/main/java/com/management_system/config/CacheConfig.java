e3fe54a3d212845d0a5d54a4558adb9d2d458267
DangDangHocCode <haidang972004@gmail.com>
2026-01-14 02:05:19 +0700

    chore(be): update cache config, application properties and pom

---
+++ management_system/src/main/java/com/management_system/config/CacheConfig.java
@@ -0,0 +1,30 @@
+package com.management_system.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("knowledgeCache");
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    private Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(30, TimeUnit.MINUTES)
+                .recordStats();
+    }
+}
